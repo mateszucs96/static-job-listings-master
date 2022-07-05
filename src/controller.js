873f5cdf52a0bbba6e633ea6a03f704187f91e2b
szucsmate <szucsmate096@gmail.com>
2022-07-05 16:38:22 +0200

    add functionality to show filter box when a keyword is clicked

--- src/controller.js
+++ src/controller.js
@@ -5,8 +5,19 @@ import Job from './view.js';
 
 const controlJobs = async () => {
     await model.fetchURL('./data.json')
-    model.state.jobs.forEach(el => Job.renderJob(el))
+    model.state.jobs.forEach(el => Job.renderJob(el, controlKeyWords))
+    Job.addHandlerKeyWords(controlKeyWords);
 };
+
+const controlKeyWords = e => {
+    Job.renderFilter();
+    Job.renderFilterKeyWords(e);
+    Job.addHandlerRemove(controlRemove);
+}
+
+const controlRemove = e => {
+    Job.removeKeyWord(e);
+}
 
 const init = () => {
     controlJobs()
--- src/view.js
+++ src/view.js
@@ -1,16 +1,56 @@
 class Job {
     data;
+    languagesCard;
     keyWords = [];
     jobs = document.querySelector('.jobs-container');
+    filterContainer = document.querySelector('.filter-container');
+    filterBox = document.querySelector('.filter-box');
+    removeIcon;
 
     renderKeyWords(data, el) {
-        this.keyWords = [data.role, data.level, ...data.languages]
+        this.keyWords = [data.role, data.level, ...data.languages];
         return `
         <div class="languages__card">
             <h3 class="title">${el}</h3>
         </div>  
         `
 
+    };
+
+    renderFilterKeyWords(e) {
+        const text = e.target.textContent.trim();
+        const html = `
+        <div class="filter-card">
+            <h3 class="filter-card__title">${text}</h3>
+            <div class="icon-box">
+                <img class="remove-icon" src="./images/icon-remove.svg" alt="remove-icon">
+            </div>
+        </div>
+        `
+        this.filterBox.insertAdjacentHTML('beforeend', html);
+        this.removeIcon = document.querySelectorAll('.remove-icon');
+    }
+
+    renderFilter() {
+        this.filterContainer.style.display = 'flex';
+    }
+
+    addHandlerKeyWords(handler) {
+        this.languagesCard.forEach(el => el.addEventListener('click', (e) => {
+
+            handler(e);
+        }));
+    };
+
+    addHandlerRemove(handler) {
+        this.removeIcon.forEach(el => el.addEventListener('click', (e) => {
+            handler(e)
+        }))
+    }
+
+    removeKeyWord(e) {
+        // console.log(e)
+        e.target.parentElement.parentElement.remove();
     }
 
     renderJob(data) {
@@ -46,8 +86,9 @@ class Job {
             </div>
     `
         this.jobs.insertAdjacentHTML('beforeend', html);
+        this.languagesCard = document.querySelectorAll('.languages__card');
 
-    }
-}
+    };
+};
 
 export default new Job();
